5b2f82bde10ba61aa3f625be665a9cef7302a71c
Elvis Wang <wangbo12@58ganji.com>
2017-11-23 11:07:10 +0800

    Add EnableScfSupport annotation support

--- access-limit-demo/src/main/java/com/bj58/arch/baseservice/accesslimit/demo/DemoServiceImpl.java
+++ access-limit-demo/src/main/java/com/bj58/arch/baseservice/accesslimit/demo/DemoServiceImpl.java
@@ -9,6 +9,11 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.CheckForNull;
+import javax.annotation.CheckForSigned;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 /**
  * TODO add brief description here
  *
@@ -17,19 +22,20 @@ import java.util.Map;
  * @author Elvis Wang [wangbo12 -AT- 58ganji -DOT- com]
  */
 @EnableAccessLimit
+//@EnableScfSupport
 public class DemoServiceImpl implements DemoService {
     private static final Logger LOGGER = LoggerFactory.getLogger(DemoServiceImpl.class);
 
     @Override
     @AccessLimit(limit = 30)
     @Deprecated
-    public void demoMethod1(int arg1, String arg2, Map<String, Long> arg3) {
+    public void demoMethod1(int arg1, String arg2, @Nonnull Map<String, Long> arg3) {
         LOGGER.info("DemoServiceImpl#demoMethod1 invoked");
     }
 
     @Override
     @AccessLimit(limit = 30, weight = 16)
-    public void demoMethod2(short arg1, byte[] arg2, List<Integer> arg3) {
+    public void demoMethod2(@CheckForSigned short arg1, @Nullable @CheckForNull byte[] arg2, List<Integer> arg3) {
         LOGGER.info("DemoServiceImpl#demoMethod2 invoked");
     }
 }
--- access-limit-processor/src/main/java/com/bj58/arch/baseservice/accesslimit/processor/impl/AccessLimitMethodConfig.java
+++ access-limit-processor/src/main/java/com/bj58/arch/baseservice/accesslimit/processor/impl/AccessLimitMethodConfig.java
@@ -1,5 +1,7 @@
 package com.bj58.arch.baseservice.accesslimit.processor.impl;
 
+import com.google.auto.value.AutoValue;
+
 /**
  * TODO add brief description here
  *
@@ -7,131 +9,30 @@ package com.bj58.arch.baseservice.accesslimit.processor.impl;
  *
  * @author Elvis Wang [wangbo12 -AT- 58ganji -DOT- com]
  */
-final class AccessLimitMethodConfig {
-    private final int index;
-    private final int limit;
-    private final int seconds;
-    private final int weight;
+@AutoValue
+abstract class AccessLimitMethodConfig {
+    abstract int index();
 
-    private AccessLimitMethodConfig(
-            int index, int limit, int seconds, int weight
-    ) {
-        this.index = index;
-        this.limit = limit;
-        this.seconds = seconds;
-        this.weight = weight;
-    }
+    abstract int limit();
 
-    int index() {
-        return index;
-    }
+    abstract int seconds();
 
-    int limit() {
-        return limit;
-    }
-
-    int seconds() {
-        return seconds;
-    }
-
-    int weight() {
-        return weight;
-    }
-
-    @Override
-    public String toString() {
-        return "AccessLimitMethodConfig{"
-                + "index=" + index + ", "
-                + "limit=" + limit + ", "
-                + "seconds=" + seconds + ", "
-                + "weight=" + weight
-                + "}";
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (o instanceof AccessLimitMethodConfig) {
-            AccessLimitMethodConfig that = (AccessLimitMethodConfig) o;
-            return (this.index == that.index())
-                    && (this.limit == that.limit())
-                    && (this.seconds == that.seconds())
-                    && (this.weight == that.weight());
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        int h = 1;
-        h *= 1000003;
-        h ^= this.index;
-        h *= 1000003;
-        h ^= this.limit;
-        h *= 1000003;
-        h ^= this.seconds;
-        h *= 1000003;
-        h ^= this.weight;
-        return h;
-    }
+    abstract int weight();
 
     public static Builder builder() {
-        return new Builder();
+        return new AutoValue_AccessLimitMethodConfig.Builder();
     }
 
-    static final class Builder {
-        private Integer index;
-        private Integer limit;
-        private Integer seconds;
-        private Integer weight;
-        Builder() {
-        }
+    @AutoValue.Builder
+    public abstract static class Builder {
+        public abstract Builder index(int index);
 
-        public AccessLimitMethodConfig.Builder index(int index) {
-            this.index = index;
-            return this;
-        }
+        public abstract Builder limit(int limit);
 
-        public AccessLimitMethodConfig.Builder limit(int limit) {
-            this.limit = limit;
-            return this;
-        }
+        public abstract Builder seconds(int seconds);
 
-        public AccessLimitMethodConfig.Builder seconds(int seconds) {
-            this.seconds = seconds;
-            return this;
-        }
+        public abstract Builder weight(int weight);
 
-        public AccessLimitMethodConfig.Builder weight(int weight) {
-            this.weight = weight;
-            return this;
-        }
-
-        public AccessLimitMethodConfig build() {
-            String missing = "";
-            if (this.index == null) {
-                missing += " index";
-            }
-            if (this.limit == null) {
-                missing += " limit";
-            }
-            if (this.seconds == null) {
-                missing += " seconds";
-            }
-            if (this.weight == null) {
-                missing += " weight";
-            }
-            if (!missing.isEmpty()) {
-                throw new IllegalStateException("Missing required properties:" + missing);
-            }
-            return new AccessLimitMethodConfig(
-                    this.index,
-                    this.limit,
-                    this.seconds,
-                    this.weight
-            );
-        }
+        public abstract AccessLimitMethodConfig build();
     }
 }
